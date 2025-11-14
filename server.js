6fa2f47b6d01a1d650a2fea9f64caa3e9eea9d73
Sebastian Robert Danci <ir3kt4fun@gmail.com>
2025-11-14 14:20:13 +0000

    Added Console Logger For Better Request Tracking

--- server.js
+++ server.js
@@ -89,6 +89,12 @@ let orders = [];
 app.use(cors());
 app.use(express.json());
 
+// Logger middleware
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // API Endpoints
 
 // GET /lessons
